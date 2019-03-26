b7d98fc2f30c77324a434ca66eb5360475889330
julimovify <julie@movify.com>
2019-03-26 16:50:55 +0100

    Add tooltip on generate button

--- app/src/components/Helper.jsx
+++ app/src/components/Helper.jsx
@@ -5,21 +5,26 @@ import PropTypes from 'prop-types';
 import QuestionMarkIcon from 'react-icons/lib/fa/question-circle-o';
 
 const Container = styled.span`
-  color: ${p => p.theme.colors.main};
+  color: ${p => (p.lightMode ? 'white' : p.theme.colors.main)};
   width: 20px;
   display: inline-flex;
   justify-content: center;
   align-items: flex-end;
 `;
 
-const Helper = ({ text }) => (
-  <Container data-tip={text}>
+const Helper = ({ lightMode, text }) => (
+  <Container data-tip={text} lightMode={lightMode}>
     <QuestionMarkIcon />
   </Container>
 );
 
 Helper.propTypes = {
+  lightMode: PropTypes.bool,
   text: PropTypes.string.isRequired,
+};
+
+Helper.defaultProps = {
+  lightMode: false,
 };
 
 export default Helper;
