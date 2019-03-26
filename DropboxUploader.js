a293e9f8bf0e8678b8bd710f4d1025c32ec03ec2
--global <--global>
2019-03-26 16:21:19 +0100

    Add DropboxUploader on server at generate doc

---
+++ DropboxUploader.js
@@ -0,0 +1,27 @@
+const Dropbox = require('dropbox').Dropbox;
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+const sharedFolderPath = '/Movify Team Folder/Apply@Movify/';
+
+const getFileName = data => {
+    const date = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+    // Naming convention: Firstname Lastname - CV Movify - Position - YYYYMMDD
+    return `CV Movify - ${data.firstName} ${data.lastName} - ${data.position} - ${date}.docx`;     
+}
+
+
+module.exports = {
+    uploadDocToDropbox(doc, data) {
+        const accessToken = process.env.REACT_APP_DROPBOX_ACCESS_TOKEN;
+        var dbx = new Dropbox({ accessToken , fetch });
+
+        dbx.filesUpload({ path: sharedFolderPath + getFileName(data), contents: doc, mode: 'add', autorename: true })
+        .then(function(response) {
+            console.log("response", response);
+        })
+        .catch(function(error) {
+            console.error("error", error.error);
+        });
+    }
+};
