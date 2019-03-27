e76b5864d6602ca8cb45d86fb0899d3619866dbe
juliemovify <julie@movify.com>
2019-03-27 16:36:52 +0100

    Add more basic info : phone email and country

--- app/src/containers/BasicInfo.jsx
+++ app/src/containers/BasicInfo.jsx
@@ -2,22 +2,35 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
+import styled from 'styled-components';
 
 import Card from '../components/Card';
 import Fieldset from '../components/Fieldset';
-import TextInput from '../components/TextInput';
+import TextInput, { Label } from '../components/TextInput';
 import Textarea from '../components/Textarea';
 import * as basicInfoActions from '../reducers/basicInfo';
+
+const countryOptions = [{ name: 'Belgium', value: 'BE' }, { name: 'Luxembourg', value: 'LU' }];
+
+const SelectContainer = styled.div`
+  display: flex;
+`;
 
 const BasicInfo = (
   {
     firstName,
     lastName,
     position,
+    email,
+    phone,
+    country,
     executiveSummary,
     setFirstName,
     setLastName,
     setPosition,
+    setEmail,
+    setPhone,
+    setCountry,
     setExecutiveSummary,
   },
 ) => (
@@ -44,6 +57,31 @@ const BasicInfo = (
         onChange={setPosition}
         required
       />
+      <TextInput
+        label="Email"
+        name="email"
+        type="email"
+        value={email}
+        onChange={setEmail}
+        required
+      />
+      <TextInput
+        label="Phone"
+        name="phone"
+        type="tel"
+        value={phone}
+        onChange={setPhone}
+        required
+      />
+      <SelectContainer>
+        <Label>Country</Label>
+        <select
+          onChange={event => setCountry(event.target.value)}
+          value={country}
+        >
+          {countryOptions.map(option => <option value={option.value}>{option.name}</option>)}
+        </select>
+      </SelectContainer>
     </Fieldset>
     <Fieldset legend="Executive summary">
       <Textarea
@@ -62,10 +100,19 @@ BasicInfo.propTypes = {
   firstName: PropTypes.string.isRequired,
   lastName: PropTypes.string.isRequired,
   position: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+  country: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+  }).isRequired,
   executiveSummary: PropTypes.string.isRequired,
   setFirstName: PropTypes.func.isRequired,
   setLastName: PropTypes.func.isRequired,
   setPosition: PropTypes.func.isRequired,
+  setEmail: PropTypes.func.isRequired,
+  setPhone: PropTypes.func.isRequired,
+  setCountry: PropTypes.func.isRequired,
   setExecutiveSummary: PropTypes.func.isRequired,
 };
 
@@ -74,12 +121,18 @@ export default connect(
     firstName: state.basicInfo.firstName,
     lastName: state.basicInfo.lastName,
     position: state.basicInfo.position,
+    email: state.basicInfo.email,
+    phone: state.basicInfo.phone,
+    country: state.basicInfo.country,
     executiveSummary: state.basicInfo.executiveSummary,
   }),
   dispatch => bindActionCreators({
     setFirstName: basicInfoActions.setFirstName,
     setLastName: basicInfoActions.setLastName,
     setPosition: basicInfoActions.setPosition,
+    setEmail: basicInfoActions.setEmail,
+    setPhone: basicInfoActions.setPhone,
+    setCountry: basicInfoActions.setCountry,
     setExecutiveSummary: basicInfoActions.setExecutiveSummary,
   }, dispatch),
 )(BasicInfo);
--- app/src/reducers/basicInfo.js
+++ app/src/reducers/basicInfo.js
@@ -2,12 +2,18 @@ const initialState = {
   firstName: '',
   lastName: '',
   position: '',
+  email: '',
+  phone: '',
+  country: 'BE',
   executiveSummary: '',
 };
 
 const SET_FIRSTNAME = 'BASIC_INFO_SET_FIRSTNAME';
 const SET_LASTNAME = 'BASIC_INFO_SET_LASTNAME';
 const SET_POSITION = 'BASIC_INFO_SET_POSITION';
+const SET_EMAIL = 'BASIC_INFO_SET_EMAIL';
+const SET_PHONE = 'BASIC_INFO_SET_PHONE';
+const SET_COUNTRY = 'BASIC_INFO_SET_COUNTRY';
 const SET_EXECUTIVE_SUMMARY = 'BASIC_INFO_SET_EXECUTIVE_SUMMARY';
 const RESET = 'BASIC_INFO_RESET';
 
@@ -28,6 +34,21 @@ const basicInfo = (state = initialState, { type, payload }) => {
         ...state,
         position: payload,
       };
+    case SET_EMAIL:
+      return {
+        ...state,
+        email: payload,
+      };
+    case SET_PHONE:
+      return {
+        ...state,
+        phone: payload,
+      };
+    case SET_COUNTRY:
+      return {
+        ...state,
+        country: payload,
+      };
     case SET_EXECUTIVE_SUMMARY:
       return {
         ...state,
@@ -47,6 +68,9 @@ export default basicInfo;
 export const setFirstName = firstName => ({ type: SET_FIRSTNAME, payload: firstName });
 export const setLastName = lastName => ({ type: SET_LASTNAME, payload: lastName });
 export const setPosition = position => ({ type: SET_POSITION, payload: position });
+export const setEmail = email => ({ type: SET_EMAIL, payload: email });
+export const setPhone = phone => ({ type: SET_PHONE, payload: phone });
+export const setCountry = country => ({ type: SET_COUNTRY, payload: country });
 export const setExecutiveSummary = executiveSummary => ({
   type: SET_EXECUTIVE_SUMMARY,
   payload: executiveSummary,
