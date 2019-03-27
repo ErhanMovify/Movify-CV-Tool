48a76f8d03db3347152ae91691851add2f1fd329
juliemovify <julie@movify.com>
2019-03-27 10:31:35 +0100

    Refactor dropbox uploader to upload with path id

--- DropboxUploader.js
+++ DropboxUploader.js
@@ -2,26 +2,26 @@ const Dropbox = require('dropbox').Dropbox;
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
-const sharedFolderPath = '/Movify Team Folder/Apply@Movify/';
-
 const getFileName = data => {
     const date = new Date().toISOString().slice(0, 10).replace(/-/g, '');
     // Naming convention: CV Movify - Firstname Lastname - YYYYMMDD - Position
     return `CV Movify - ${data.firstName} ${data.lastName} - ${date} - ${data.position}.docx`;     
 }
 
+const uploadFile = (file, accessToken) => {
+    var dbx = new Dropbox({ accessToken , fetch });
+
+    dbx.filesUpload({ path: file.path, contents: file.data, mode: 'add', autorename: true })
+    .then(response => console.log(response))
+    .catch(error => console.error(error.error));
+}
 
 module.exports = {
     uploadDocToDropbox(doc, data) {
         const accessToken = process.env.NODE_APP_DROPBOX_ACCESS_TOKEN;
-        var dbx = new Dropbox({ accessToken , fetch });
-
-        dbx.filesUpload({ path: sharedFolderPath + getFileName(data), contents: doc, mode: 'add', autorename: true })
-        .then(function(response) {
-            console.log("response", response);
-        })
-        .catch(function(error) {
-            console.error("error", error.error);
-        });
+        const folderId = 'id:m41NXAS4SBAAAAAAAACR7w';
+        const filePath = `${folderId}/${getFileName(data)}`;
+        
+        uploadFile({ data: doc, path: filePath }, accessToken);
     }
 };
